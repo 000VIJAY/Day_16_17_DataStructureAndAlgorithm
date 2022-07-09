2698a2446b41dd52462a32cd18d4c6961ad0870c
000VIJAY <vijay893689@gmail.com>
2022-07-09 18:42:14 +0530

    Vijay Added program for Permutation of String

---
+++ AlgorithmAndDsProgrammingProblem/Permutation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAndDsProgrammingProblem
+{
+    public class Permutation
+    {
+        public static void permute(String a ,int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(a);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    a = swap(a, l, i);
+                    permute(a, l + 1, r);
+                }
+            }
+        }
+        public static String swap(String b, int i, int j)
+        {
+            char temp;
+            char[] charArray = b.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
---
+++ AlgorithmAndDsProgrammingProblem/Program.cs
@@ -0,0 +1,4 @@
+﻿using AlgorithmAndDsProgrammingProblem;
+String str = "ABC";
+int n = str.Length;
+Permutation.permute(str, 0, n - 1);
