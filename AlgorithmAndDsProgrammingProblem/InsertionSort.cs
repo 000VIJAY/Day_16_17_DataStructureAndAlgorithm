ff4bcaba6996002fc1ade78b44e9e04d3d4a1608
000VIJAY <vijay893689@gmail.com>
2022-07-13 18:26:11 +0530

    Added Program for Insertion Sort

---
+++ AlgorithmAndDsProgrammingProblem/InsertionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AlgorithmAndDsProgrammingProblem
+{
+    public class Insertion
+    {
+        public static void InsertionSort(string sentence)
+        {
+            string[] word = sentence.Split(' ');
+            for (int i = 1; i < (word.Length) - 1; i++)
+            {
+                int j = i;
+                string key = word[i];
+
+
+                while (word[j - 1].CompareTo(key) > 0)
+                {
+                    word[j] = word[j - 1];
+                    j--;
+                    if (j == 0)
+                    {
+                        break;
+                    }
+                }
+                word[j] = key;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(word[i] + " ");
+            }
+        }
+    }
+}
--- AlgorithmAndDsProgrammingProblem/Program.cs
+++ AlgorithmAndDsProgrammingProblem/Program.cs
@@ -1,8 +1,4 @@
 ﻿using AlgorithmAndDsProgrammingProblem;
-BinarySearch Search = new BinarySearch();
-Console.WriteLine("Hello, to the algorithm and data structure problems");
 Console.WriteLine("Enter a sentence");
 String sentence = Console.ReadLine();
-Console.WriteLine("Enter the word you want to search");
-String search = Console.ReadLine();
-Search.SortingAndCalling(sentence,search);
+Insertion.InsertionSort(sentence);
